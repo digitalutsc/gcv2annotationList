48a229bef5c346f7dfe1be5ac38aabc674a13636
AndrewEffendi <skz@ZenBookDuo.localdomain>
2022-03-25 13:32:37 -0400

    gcv2anno v1

--- gcv2anno.java
+++ gcv2anno.java
@@ -2,6 +2,7 @@ package gcv2anno;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
@@ -10,29 +11,121 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class gcv2anno {
-    public static void main(String[] args) throws IOException, ParseException{
-        JSONParser parser = new JSONParser();
-        JSONArray a = (JSONArray) parser.parse(new FileReader(args[0]));
+    private static FileWriter file;
 
-        for (Object o : a)
-        {
-          JSONObject person = (JSONObject) o;
-      
-          String name = (String) person.get("name");
-          System.out.println(name);
-      
-          String city = (String) person.get("city");
-          System.out.println(city);
-      
-          String job = (String) person.get("job");
-          System.out.println(job);
-      
-          JSONArray cars = (JSONArray) person.get("cars");
-      
-          for (Object c : cars)
-          {
-            System.out.println(c+"");
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IOException, ParseException{
+        final String INPUT_PATH = args[0];
+        final String OUTPUT_PATH = args[1];
+        final String CANVAS = args[2];
+        // read the json file and parse it
+        JSONParser parser = new JSONParser();
+        JSONObject inJSONObject = (JSONObject) parser.parse(new FileReader(INPUT_PATH));
+
+        // create new JSONObject to write output
+        JSONObject outJSONObject = new JSONObject();
+        outJSONObject.put("@context", "http://iiif.io/api/presentation/2/context.json");
+        outJSONObject.put("@id", "https://islandora.traefik.me/anolist.json");
+        outJSONObject.put("@type", "sc:AnnotationList");
+        JSONArray resources = new JSONArray();
+
+        // Use the JSONObject
+        JSONArray pages = (JSONArray) ((JSONObject) inJSONObject.get("fullTextAnnotation")).get("pages");
+        for(Object page: pages){
+          JSONArray blocks = (JSONArray) ((JSONObject)page).get("blocks");
+          for(Object block: blocks){
+            JSONArray paragraphs = (JSONArray) ((JSONObject)block).get("paragraphs");
+            for(Object paragraph: paragraphs){
+              JSONArray vertices = (JSONArray)(((JSONObject)((JSONObject)paragraph).get("boundingBox"))).get("vertices");
+              int i = 1;
+              long x = 0;
+              long y = 0;
+              long xx = 0;
+              long yy = 0;
+              for(Object vertice: vertices){
+                if(i==1){
+                  // get the coordinates of the top left corner of the bounding box
+                  x = (long) ((JSONObject)vertice).get("x");
+                  y = (long) ((JSONObject)vertice).get("y");
+                }else if(i==3){
+                  // get the coordinates of the bottom right corner of the bounding box
+                  xx = (long) ((JSONObject)vertice).get("x");
+                  yy = (long) ((JSONObject)vertice).get("y");
+                }
+                i++;
+              }
+              // calculate the width and height
+              long w = xx - x;
+              long h = yy - y;
+
+              /*
+              System.out.println(x);
+              System.out.println(y);
+              System.out.println(w);
+              System.out.println(h);
+              */
+
+              // get the content
+              String content = "";
+              JSONArray words = (JSONArray) ((JSONObject)paragraph).get("words");
+              for(Object word: words){
+                JSONArray symbols = (JSONArray) ((JSONObject)word).get("symbols");
+                for(Object symbol: symbols){
+                  String text = (String) ((JSONObject)symbol).get("text");
+                  content += text;
+                  // add space between words if break detected
+                  if(((JSONObject)symbol).get("property") != null){
+                    JSONObject property = (JSONObject) ((JSONObject)symbol).get("property");
+                      if(((String) ((JSONObject)((JSONObject)property).get("detectedBreak")).get("type")).equals("SPACE"))
+                        content += ' ';
+                  }
+                }
+              }
+              System.out.println(content);
+              
+              // add the information retrived to the resourcesObject
+              JSONObject resourcesObject = new JSONObject();
+              resourcesObject.put("@id", "http://localhost:69/annotation/"+ paragraph.hashCode());
+              resourcesObject.put("@type", "oa:Annotation");
+              resourcesObject.put("@context", "http://iiif.io/api/presentation/2/context.json");
+              resourcesObject.put("on", CANVAS + "#xywh=" + x + "," + y + "," + w + "," + h);
+              resourcesObject.put("label", content);
+
+              JSONObject rObj = new JSONObject();
+              rObj.put("@type", "dctypes:Text");
+              rObj.put("http://dev.llgc.org.uk/sas/full_text", content);
+              rObj.put("format", "text/html");
+              rObj.put("chars", "<p>" + content + "</p>");
+              resourcesObject.put("resource", rObj);
+              
+              JSONArray motivationObject = new JSONArray();
+              motivationObject.add("oa:commenting");
+              resourcesObject.put("motivation", motivationObject);
+              
+              resources.add(resourcesObject);
+            }
           }
         }
+        // add the resourcess JSONArray to the outJSONObject
+        outJSONObject.put("resources", resources);
+
+        // write the outJSONObject to a file
+        try {
+          // Constructs a FileWriter given a file name, using the platform's default charset
+          file = new FileWriter(OUTPUT_PATH);
+          file.write(outJSONObject.toJSONString().replace("\\/","/"));
+      } catch (IOException e) {
+          e.printStackTrace();
+
+      } finally {
+
+          try {
+              file.flush();
+              file.close();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }
+        System.out.println("done");
     }
 }
