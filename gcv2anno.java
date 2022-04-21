d8d71ed3cc425efb25a58e56914bd9f32c10b549
AndrewEffendi <skz@ZenBookDuo.localdomain>
2022-04-21 12:53:22 -0400

    add comments to file

--- gcv2anno.java
+++ gcv2anno.java
@@ -9,6 +9,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.net.*;
 
 public class gcv2anno {
     private static FileWriter file;
@@ -18,18 +19,21 @@ public class gcv2anno {
         final String INPUT_PATH = args[0];
         final String OUTPUT_PATH = args[1];
         final String CANVAS = args[2];
-        // read the json file and parse it
+
+        // read the json file and parse it to a json object
         JSONParser parser = new JSONParser();
         JSONObject inJSONObject = (JSONObject) parser.parse(new FileReader(INPUT_PATH));
 
-        // create new JSONObject to write output
+        // create new JSONObject to store the output
         JSONObject outJSONObject = new JSONObject();
+        // store the context, id, and type to the output JSONObject
         outJSONObject.put("@context", "http://iiif.io/api/presentation/2/context.json");
         outJSONObject.put("@id", "https://islandora.traefik.me/" + OUTPUT_PATH);
         outJSONObject.put("@type", "sc:AnnotationList");
-        JSONArray resources = new JSONArray();
+        // initialize resourcesArray (the array that stores the annotations)
+        JSONArray resourcesArray = new JSONArray();
 
-        // Use the JSONObject
+        // Use the input JSONObject to get the contents, height, width, and the x-y coordinates of the top left corner of the bounding box
         JSONArray pages = (JSONArray) ((JSONObject) inJSONObject.get("fullTextAnnotation")).get("pages");
         for(Object page: pages){
           JSONArray blocks = (JSONArray) ((JSONObject)page).get("blocks");
@@ -54,16 +58,10 @@ public class gcv2anno {
                 }
                 i++;
               }
-              // calculate the width and height
+              // calculate the width by substracting the x coordinate of the bottom right corner with x coordinate of the top left corner
               long w = xx - x;
+              // calculate the height by substracting the y coordinate of the bottom right corner with y coordinate of the top left corner
               long h = yy - y;
-
-              /*
-              System.out.println(x);
-              System.out.println(y);
-              System.out.println(w);
-              System.out.println(h);
-              */
 
               // get the content
               String content = "";
@@ -81,51 +79,51 @@ public class gcv2anno {
                   }
                 }
               }
-              System.out.println(content);
-              
+
+              // find a free port for the id of the annotation (passing 0 to ServerSocket constructor will make it listen on any free port.)
+              // it doesn't matter which port is used as long as its free
+              ServerSocket serverSocket = new ServerSocket(0);
+
               // add the information retrived to the resourcesObject
               JSONObject resourcesObject = new JSONObject();
-              resourcesObject.put("@id", "http://localhost:69/annotation/"+ paragraph.hashCode());
+              resourcesObject.put("@id", "http://localhost:" + serverSocket.getLocalPort() + "/annotation/"+ paragraph.hashCode());
               resourcesObject.put("@type", "oa:Annotation");
               resourcesObject.put("@context", "http://iiif.io/api/presentation/2/context.json");
               resourcesObject.put("on", CANVAS + "#xywh=" + x + "," + y + "," + w + "," + h);
               resourcesObject.put("label", content);
-
               JSONObject rObj = new JSONObject();
               rObj.put("@type", "dctypes:Text");
               rObj.put("http://dev.llgc.org.uk/sas/full_text", content);
               rObj.put("format", "text/html");
               rObj.put("chars", "<p>" + content + "</p>");
               resourcesObject.put("resource", rObj);
-              
               JSONArray motivationObject = new JSONArray();
               motivationObject.add("oa:commenting");
               resourcesObject.put("motivation", motivationObject);
               
-              resources.add(resourcesObject);
+              // add the resourcesObject to the resourcesArray
+              resourcesArray.add(resourcesObject);
             }
           }
         }
-        // add the resourcess JSONArray to the outJSONObject
-        outJSONObject.put("resources", resources);
+        // add the resourcesArray to the outJSONObject
+        outJSONObject.put("resources", resourcesArray);
 
         // write the outJSONObject to a file
         try {
           // Constructs a FileWriter given a file name, using the platform's default charset
           file = new FileWriter(OUTPUT_PATH);
           file.write(outJSONObject.toJSONString().replace("\\/","/"));
-      } catch (IOException e) {
+        } catch (IOException e) {
           e.printStackTrace();
-
-      } finally {
-
+        } finally {
           try {
-              file.flush();
-              file.close();
+            file.flush();
+            file.close();
           } catch (IOException e) {
-              e.printStackTrace();
+            e.printStackTrace();
           }
-      }
+        }
         System.out.println("done");
     }
 }
